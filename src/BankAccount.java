b95d04f5ab275288f10f93e14dfe98c6867fa34c
Ruby Lopez <141886191+rlope234@users.noreply.github.com>
2025-01-30 23:22:50 +0000

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
 
+public class BankAccount{
+    public static void main(String[] args) {
+        float start = 2175.37f; // starting balance
+        float withdrew = start - 50.03f;
+        float part3 = (withdrew / 2) + 20.00f;
+        float part4 = part3 - 1f;
+        float part5 = part4 * 2f;
+        float part6 = part5 + 1f;
+
+        
+        System.out.println("Bob's new balance after all transaction: " + part6);
+    }
+}
